715ed65c095273b5644fcf906c5af785cf60dc73
Allen Chau <achau7704@gmail.com>
2026-03-30 17:02:07 -0700

    add local backward op and tests

--- src/nanotorch/value.py
+++ src/nanotorch/value.py
@@ -8,16 +8,82 @@ class Value:
         # parents for 
         self.parents = ()
         self.op = ""
+        # every node should have a _backward function; for leaf nodes we have a function that does nothing
+        self._backward = lambda: None
+
+    def __repr__(self):
+        return f"Data: {self.data}, Grad: {self.grad}, Op: {self.op}"
 
     def __add__(self, other):
-        c = Value(self.data + other.data)
-        c.op = "+"
-        c.parents = ()
-        return c
+        res = Value(self.data + other.data)
+        res.op = "+"
+        res.parents = (self, other)
 
-    def __mul__(self, a, b):
-        c = Value(a.data * b.data)
-        return c
+        # define the backward pass for addition
+        def add_backward():
+            self.grad += res.grad
+            other.grad += res.grad
+
+        res._backward = add_backward
+        return res
+
+    def __sub__(self, other):
+        res = Value(self.data - other.data)
+        res.op = "-"
+        res.parents = (self, other)
+
+        # define the backward pass for subtraction
+        def sub_backward():
+            self.grad += res.grad
+            other.grad += -res.grad
+
+        res._backward = sub_backward
+        return res
+
+    def __mul__(self, other):
+        res = Value(self.data * other.data)
+        res.op = "*"
+        res.parents = (self, other)
+
+        # define the backward pass for multiplication
+        def mul_backward():
+            self.grad += res.grad * other.data
+            other.grad += res.grad* self.data
+
+        res._backward = mul_backward
+        return res
+
+    def __div__(self, other):
+        res = Value(self.data / other.data)
+        res.op = "/"
+        res.parents = (self, other)
+
+        # define the backward pass for division
+        def div_backward():
+            self.grad += res.grad
+            other.grad += -res.grad
+
+        res._backward = div_backward
+        return res
+
+    # backprop algo
+    def backward(self):
+        # if this is our output or final loss node, dL/dL = 1
+        self.grad = 1
+        
+        # first we build a topological ordering from output node
+        def dfs(node):
+            order = []
+            if not node:
+                return
+            for parent in node.parents:
+                dfs(parent)
+            return order
+
+        # we loop over nodes and call _backward for each node in the graph
+        for node in nodes:
+            node._backward()
+        
 
     
     
--- tests/test_value_autograd.py
+++ tests/test_value_autograd.py
@@ -1,9 +1,53 @@
+from calendar import c
 import pytest
 from nanotorch.value import Value
 
-def test_scalar_addition():
+def test_scalar_addition_forward():
     a = Value(5)
     b = Value(6)
     c = a + b
     assert c.data == 11
+    assert a in c.parents and b in c.parents
+    print(c)
+
+def test_scalar_multiplication_forward():
+    a = Value(5)
+    b = Value(6)
+    c = a * b
+    assert c.data == 30
+    assert a in c.parents and b in c.parents
+    print(c)
+
+def test_scalar_addition_local_backward():
+    a = Value(5)
+    b = Value(6)
+    c = a + b
+
+    # arbitrarily set the grad to a number and see if the parent grads follow the math
+    c.grad = 60
+    c._backward()
+    assert a.grad == 60
+    assert b.grad == 60
+    assert a in c.parents and b in c.parents
+
+def test_scalar_multiplication_local_backward():
+    a = Value(5)
+    b = Value(6)
+    c = a * b
+
+    # arbitrarily set the grad to a number and see if the parent grads follow the math
+    c.grad = 60
+    c._backward()
+    assert a.grad == 360
+    assert b.grad == 300
+    assert a in c.parents and b in c.parents
+
+def test_global_backward():
+    a = Value(5)
+    b = Value(6)
+    c = Value(7)
+    d = a * b + c
+    assert a.grad == 
+    assert b.grad == 
+    assert c.grad == 
 
